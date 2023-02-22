838bfbf2a6b65c5c2c5c7ef2786558ec57cfebe9
Mark Berryman <mark.berryman@roq.co.uk>
2023-02-22 14:13:32 +0000

    Completed loop on Java

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -1,15 +1,102 @@
 package com.credersi.animals;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertTrue; //same as expect to on jest
 import org.junit.Test;
 
 public class CatTest {
-	@Test
+	@Test //defines as an automated test. Must be put before each part of test. 
 	public void testInitialState() {
 		Cat cat = new Cat();
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	@Test
+	public void testIfMoreHungry() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		assertEquals(cat.howHungry(), 50);
+	}
+	@Test
+	public void testCatCanRun() {
+		Cat cat= new Cat();
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	@Test
+	public void testCatCanFeed() {
+		Cat cat= new Cat();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 30);
+	}
+	@Test
+	public void testCatIsFull() {
+		Cat cat= new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+		}
+	@Test
+	public void testZombieCat() {
+		Cat cat= new Cat();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		
+		assertEquals(cat.howHungry(), 80);
+		assertTrue(cat.isAlive());
+		}
+	@Test
+	public void CatDies() {
+		Cat cat= new Cat();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		
+		assertTrue(!cat.isAlive());
+	}
+	@Test
+	public void CatRunsLoop() {
+		Cat cat= new Cat();
+		for(int i=0; i< 8;i++) {
+			cat.runs();
+			cat.runs();
+			cat.runs();
+			cat.runs();
 }
+		assertTrue(!cat.isAlive());
+	}
+}
+	
+
+	
+	
+
